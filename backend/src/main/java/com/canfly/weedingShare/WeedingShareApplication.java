4f744528a3e1ae490123af837ebd12920495c939
Maxime <maxime.belouin@gmail.com>
2025-03-09 18:06:11 +0100

    Création du backend Springboot

---
+++ backend/src/main/java/com/canfly/weedingShare/WeedingShareApplication.java
@@ -0,0 +1,13 @@
+package com.canfly.weedingShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedingShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeedingShareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/canfly/weedingShare/WeedingShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canfly.weedingShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeedingShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
